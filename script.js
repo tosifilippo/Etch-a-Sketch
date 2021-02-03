aca3ef99d3282e198c45534abc670e623a662ad3
filippo <tosifilippo@outlook.it>
2021-02-03 23:16:04 +0100

    added buttons for switching hover effect, separated mousehover function from makerow function, updated mousehover function with new effects, added event listener for the color buttons, created function to change color variable when buttons are clicked

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const container = document.getElementById("container");
+const colorButtons = document.querySelectorAll('.color-btns');
 
 let cell = document.getElementsByClassName("grid-item");
+let color = "black";
+
+colorButtons.forEach(colorButton => colorButton.addEventListener('click', changeColor));
+
+makeRows(16, 16);
 
 function makeRows(rows, cols) {
 	container.style.setProperty('--grid-rows', rows);
@@ -10,21 +16,55 @@ function makeRows(rows, cols) {
 		// cell.innerText = (c + 1);
 		container.appendChild(cell).className = "grid-item";
 	};
+	mouseHover();
+};
+
+function mouseHover() {
 	for (let i = 0; i < cell.length; i++) {
-    cell[i].addEventListener("mouseover", function() {
-		this.style.backgroundColor = "black"
-		// this.style.backgroundColor = "#" + ((1<<24)*Math.random() | 0).toString(16);
-		// let currentOpacity = Number(this.style.backgroundColor.slice(-4, -1));
-        // this.style.backgroundColor = `rgba(0, 0, 0, ${currentOpacity + 0.1})`;
+		cell[i].addEventListener("mouseover", function() {
+			switch (color) {
+				case 'rainbow':
+					this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+					break;  
+				case 'gray':
+					if (this.style.backgroundColor.match(/rgba/)) {
+						let currentOpacity = Number(this.style.backgroundColor.slice(-4, -1));
+						if (currentOpacity <= 0.9) {
+							this.style.backgroundColor = `rgba(0, 0, 0, ${currentOpacity + 0.1})`;
+							this.classList.add('gray');
+						}
+						} else if (this.classList == 'gray' && this.style.backgroundColor == 'rgb(0, 0, 0)') {
+							return;
+						} else {
+							this.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';  
+						}
+					break;
+				case 'black':
+					this.style.backgroundColor = '#000000';
+					break;
+				// default:
+				// 	this.style.backgroundColor = color;
+				// 	break;
+			}; 	
     	});
 	};
 };
 
-makeRows(16, 16);
+function changeColor(event) {
+    switch (event.target.dataset.color) { 
+        case 'rainbow':
+            color = 'rainbow';
+            break;  
+        case 'gray':
+            color = 'gray';
+            break;
+        default:
+            color = 'black';
+            break;
+    }; 
+};
 
-
-
-function myFunction() {
+function refresh() {
 	while (container.firstChild) {
 		container.removeChild(container.firstChild);
 		};
@@ -39,4 +79,3 @@ function myFunction() {
 	makeRows(gridSize, gridSize);
 	};
 };
-
